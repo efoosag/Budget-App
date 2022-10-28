54d3008f7c575a29b4247f838f936b4090979e73
efoosag <efoosagsite@gmail.com>
2022-10-28 06:21:31 +0100

    change to single-quoted string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u|
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :surname, :email, :password, :password_confirmation)
-    }
+    end
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :password, :current_password) }
   end
 end
